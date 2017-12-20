4dcf8f2e8fcac5faaa6e48db2ad782274ced761f
Mesalu <samuel@mesalu.com>
2017-12-19 19:12:50 -0800

    Add some more crap to the EntryFactory
    Currently supports Entry, ItemEntry, and WeaponEntry.
    
    SkillEntry will come next, followed by the rest as they get implemented.

--- app/src/main/java/com/monsalachai/dndthing/entry/EntryFactory.java
+++ app/src/main/java/com/monsalachai/dndthing/entry/EntryFactory.java
@@ -155,11 +155,12 @@ public class EntryFactory {
          */
         public EntryBuilder setTypeItem() {
             // don't change real type if typeid indicates a subtype
-            // (presently: weapon)
-            if (mBuildSite.get("typeid").getAsInt() == 2)
-                return this;    // don't change type.
+            // (presently: weapon(2))
+            if (mBuildSite.get("typeid").getAsInt() != 2)
+                mBuildSite.addProperty("typeid", 1);
 
-            mBuildSite.addProperty("typeid", 1);
+
+            noOverwrite("item");
             return this;
         }
 
@@ -172,16 +173,21 @@ public class EntryFactory {
          */
         public EntryBuilder setTypeWeapon() {
             mBuildSite.addProperty("typeid", 2);
+            noOverwrite("weapon");
+
+            // make sure that Item information is present too:
+            setTypeItem();
+
             return this;
         }
 
         /**
          * Sets the Entry's type to reflect that it is a Skill
-         *
          * @return this
          */
         public EntryBuilder setTypeSkill() {
             mBuildSite.addProperty("typeid", 3);
+            noOverwrite("skill");
             return this;
         }
 
@@ -192,6 +198,7 @@ public class EntryFactory {
          */
         public EntryBuilder setTypeCharacter() {
             mBuildSite.addProperty("typeid", 4);
+            noOverwrite("character");
             return this;
         }
 
@@ -202,6 +209,7 @@ public class EntryFactory {
         public EntryBuilder setTypeAttribute()
         {
             mBuildSite.addProperty("typeid", 5);
+            noOverwrite("attribute");
             return this;
         }
 
@@ -212,6 +220,7 @@ public class EntryFactory {
         public EntryBuilder setTypeSpell()
         {
             mBuildSite.addProperty("typeid", 6);
+            noOverwrite("spell");
             return this;
         }
 
@@ -222,7 +231,186 @@ public class EntryFactory {
         public EntryBuilder setTypeFeat()
         {
             mBuildSite.addProperty("typeid", 7);
+            noOverwrite("feat");
             return this;
+        }
+
+        /**
+         * Sets the ItemEntry's durability to the given value.
+         * @param value the value of the items durability
+         * @return this
+         */
+        public EntryBuilder addItemDurability(int value)
+        {
+            setTypeItem();
+            mBuildSite.getAsJsonObject("item").addProperty("durability", value);
+            return this;
+        }
+
+        /**
+         * Sets the ItemEntry's weight to the given value.
+         * @param value The item's weight
+         * @return this
+         */
+        public EntryBuilder addItemWeight(int value)
+        {
+            setTypeItem();
+            mBuildSite.getAsJsonObject("item").addProperty("weight", value);
+            return this;
+        }
+
+        /**
+         * Sets the ItemEntry's count to the given value (IOW: "how many of these items are
+         * in the inventory")
+         * @param value the amount of these items.
+         * @return this
+         */
+        public EntryBuilder addItemCount(int value)
+        {
+            setTypeItem();
+            mBuildSite.getAsJsonObject("item").addProperty("count", value);
+            return this;
+        }
+
+        /**
+         * Sets if this ItemEntry represents a consumable item.
+         * @return this
+         */
+        public EntryBuilder setItemConsumable()
+        {
+            setTypeItem();
+            mBuildSite.getAsJsonObject("item").addProperty("consumable", true);
+            return this;
+        }
+
+        /**
+         * Sets if this ItemEntry represents a wondrous item.
+         * @return this
+         */
+        public EntryBuilder setItemWondrous()
+        {
+            setTypeWeapon();
+            mBuildSite.getAsJsonObject("item").addProperty("wondrous", true);
+            return this;
+        }
+
+
+        /**
+         * Sets the WeaponEntry's weapon type to Magical
+         */
+        public EntryBuilder setWeaponMagical()
+        {
+            setTypeWeapon();
+            mBuildSite.getAsJsonObject("weapon").addProperty("type", "Magical");
+            return this;
+        }
+
+        public EntryBuilder setWeaponMelee()
+        {
+            setTypeWeapon();
+            mBuildSite.getAsJsonObject("weapon").addProperty("type", "Melee");
+            return this;
+        }
+
+        public EntryBuilder setWeaponRanged()
+        {
+            setTypeWeapon();
+            mBuildSite.getAsJsonObject("weapon").addProperty("type", "Ranged");
+            return this;
+        }
+
+        /**
+         * Sets the type The WeaponEntry's damage type to Slashing
+         * @return this
+         */
+        public EntryBuilder setWeaponSlashing()
+        {
+            setTypeWeapon();
+            mBuildSite.getAsJsonObject("weapon").addProperty("damageType", "Slashing");
+            return this;
+        }
+
+        public EntryBuilder setWeaponPiercing()
+        {
+            setTypeWeapon();
+            mBuildSite.getAsJsonObject("weapon").addProperty("damageType", "Piercing");
+            return this;
+        }
+
+        public EntryBuilder setWeaponBludgeoning()
+        {
+            setTypeWeapon();
+            mBuildSite.getAsJsonObject("weapon").addProperty("damageType", "Bludgeoning");
+            return this;
+        }
+
+        public EntryBuilder addAmmoCount(int count)
+        {
+            setTypeWeapon();
+            mBuildSite.getAsJsonObject("weapon").addProperty("ammo", count);
+            return this;
+        }
+
+        public EntryBuilder addAmmoTypeDescription(String desc)
+        {
+            setTypeWeapon();
+            mBuildSite.getAsJsonObject("weapon").addProperty("ammoType", desc);
+            return this;
+        }
+
+        /**
+         * Adds the given object to buildsite if the action would not otherwise overwrite data.
+         * @param label the label in mBuildSite to add to.
+         * @param object the JsonObject to add.
+         */
+        private void noOverwrite(String label, JsonObject object)
+        {
+            noOverwrite(mBuildSite, label, object);
+        }
+
+        /**
+         * Functions as above, but on a specified json object.
+         * @param label the label in json to add in as.
+         * @param object the object to add to json.
+         */
+        private void noOverwrite(JsonObject json, String label, JsonObject object)
+        {
+            try
+            {
+                json.get(label);
+            }
+            catch (JsonParseException e)
+            {
+                // actually add object
+                json.add(label, object);
+            }
+        }
+
+        /**
+         * As the other two noOverwrite methods, but will assume
+         * that object will be a brand new JSON object, and will not
+         * allocate it before determining that it will be used.
+         * @param label the label to add the new json object to.
+         */
+        private void noOverwrite(JsonObject json, String label)
+        {
+            try
+            {
+                json.get(label);
+            }
+            catch (JsonParseException e)
+            {
+                json.add(label, new JsonObject());
+            }
+        }
+
+        /**
+         * As above, but assumes target is mBuildSite
+         * @param label the label to add the new json object to.
+         */
+        private void noOverwrite(String label)
+        {
+            noOverwrite(mBuildSite, label);
         }
     }
 
