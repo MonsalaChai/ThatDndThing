f2f3c7f9a6b82d1b832d28f815d830f9f4989783
Mesalu <samuel@mesalu.com>
2017-12-22 18:55:46 -0800

    Remove DummyContent

--- app/src/main/java/com/monsalachai/dndthing/TabListActivity.java
+++ app/src/main/java/com/monsalachai/dndthing/TabListActivity.java
@@ -7,16 +7,10 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-
-import com.monsalachai.dndthing.dummy.DummyContent;
 
 import java.util.ArrayList;
 import java.util.List;
